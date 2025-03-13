0398b8b71cad5fbbc54724325c7745bc92eadc53
Nwitlyck <davidsegura118@hotmail.com>
2025-03-13 12:28:26 -0600

    Semana 9-A

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+
+    public CharacterController player;
+
+    public float playerSpeed;
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+    public float fallVelocity = 40;
+
+    public Camera mainCamera;
+    private Vector3 camFoward;
+    private Vector3 camRight;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camFoward;
+
+        movePlayer = movePlayer * playerSpeed;
+        
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        SetGravity();
+
+        player.Move(movePlayer * Time.deltaTime);
+    }
+
+    void camDirection()
+    {
+        camFoward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camFoward.y = 0;
+        camRight.y = 0;
+
+        camFoward = camFoward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    void SetGravity()
+    {
+        if (player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+    }
+}
